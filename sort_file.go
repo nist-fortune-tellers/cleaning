97c939ac1717c2d76436828dbd515175993be885
Max Fresonke <max@fresonke.com>
2015-12-01 05:48:48 -0500

    All Done With Go Sorting

--- sort_file.go
+++ sort_file.go
@@ -7,13 +7,11 @@ import (
 	"io"
 	"log"
 	"os"
-	"sort"
 	"strconv"
 	"strings"
-	"time"
 )
 
-const outputCompleteData = true
+const outputCompleteData = false
 
 func main() {
 	args := os.Args[len(os.Args)-3:]
@@ -30,61 +28,21 @@ func main() {
 	log.Println("Scanning ", fileToSort, " Into Memory")
 	dps := scanFile(numLines, fileToSort)
 
-	log.Println("Sorting...")
-	sort.Sort(multiSorter{dps})
-
 	log.Println("Writing File")
-	writeFile(dps, outputFile)
+	writeFile(ids, dps, outputFile)
 	log.Println("Done!")
 }
 
+type laneID int
+type dataMap map[laneID]*dataPoint
+
 //Lane is an abstract representation of a single datapoint in the file.
 type dataPoint struct {
-	splits   []string
-	laneID   int
-	unixTime int64
-}
-
-type dataPoints []dataPoint
-
-func (dps dataPoints) Len() int {
-	return len(dps)
-}
-
-func (dps dataPoints) Swap(i, j int) {
-	dps[i], dps[j] = dps[j], dps[i]
-}
-
-func (dps dataPoints) idLess(i, j int) bool {
-	return dps[i].laneID < dps[j].laneID
-}
-
-func (dps dataPoints) timeLess(i, j int) bool {
-	return dps[i].unixTime < dps[j].unixTime
-}
-
-type multiSorter struct{ dataPoints }
-
-// Less is part of sort.Interface. It is implemented by looping along the
-// less functions until it finds a comparison that is either Less or
-// !Less. Note that it can call the less functions twice per call. We
-// could change the functions to return -1, 0, 1 and reduce the
-// number of calls for greater efficiency: an exercise for the reader.
-func (ms multiSorter) Less(i, j int) bool {
-	switch {
-	//first priority is date
-	case ms.dataPoints.timeLess(i, j):
-		return true
-	case ms.dataPoints.timeLess(j, i):
-		return false
-	}
-	//otherwise, just return the last comparison.
-	return ms.dataPoints.idLess(i, j)
-
+	splits []string
 }
 
 //NewLane makes a new init'd lane object from a line.
-func newDataPoint(line string) dataPoint {
+func newDataPoint(line string) (laneID, *dataPoint) {
 	//Example Format:
 	//Mon Jan 2 15:04:05 -0700 MST 2006
 	//2011-05-01 00:17
@@ -93,16 +51,10 @@ func newDataPoint(line string) dataPoint {
 	splits := strings.Split(line, "\t")
 	//Example Line:
 	// 10056	2011-05-01 00:17	0	30	2
-	laneID, err := strconv.Atoi(splits[0])
-	check(err)
-	time, err := time.Parse(timeFormat, splits[1])
+	laneIDint, err := strconv.Atoi(splits[0])
 	check(err)
 
-	return dataPoint{
-		splits:   splits,
-		laneID:   laneID,
-		unixTime: time.Unix(),
-	}
+	return laneID(laneIDint), &dataPoint{splits}
 }
 
 func (dp dataPoint) writeLines(w *bufio.Writer) {
@@ -130,21 +82,22 @@ func check(err error) {
 	}
 }
 
-func writeFile(dps dataPoints, output string) {
+func writeFile(ids []laneID, dps dataMap, output string) {
 	file, err := os.Create(output)
 	check(err)
 	defer file.Close()
 	bw := bufio.NewWriter(file)
 	defer bw.Flush()
-	for _, dp := range dps {
+	for _, id := range ids {
+		dp := dps[id]
 		dp.writeLines(bw)
 		bw.WriteString("\n")
 	}
 }
 
-func scanFile(numLines int, fileToScan string) dataPoints {
+func scanFile(numLines int, fileToScan string) dataMap {
 	//create a slice with the number of lines needed.
-	dps := make(dataPoints, 0, numLines)
+	dps := make(dataMap, int(float64(numLines)*1.1))
 	//open the file for reading.
 	file, err := os.Open(fileToScan)
 	check(err)
@@ -153,16 +106,17 @@ func scanFile(numLines int, fileToScan string) dataPoints {
 	//Start scanning line-by-line
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		dps = append(dps, newDataPoint(scanner.Text()))
+		key, dp := newDataPoint(scanner.Text())
+		dps[key] = dp
 	}
 	return dps
 }
 
-func getIDList(input string) []int {
+func getIDList(input string) []laneID {
 	numLines, err := lineCounter(input)
 	check(err)
 	fmt.Println("Number of Lines in Original Input File: ", numLines)
-	ids := make([]int, 0, numLines)
+	ids := make([]laneID, 0, numLines)
 	//open the file for reading.
 	file, err := os.Open(input)
 	check(err)
@@ -172,13 +126,13 @@ func getIDList(input string) []int {
 	scanner := bufio.NewScanner(file)
 	//skip first line
 	scanner.Scan()
-	fmt.Println("Retrieving List of Indexes")
+	fmt.Println("Retrieving List of Indexes from ", input)
 	for scanner.Scan() {
 		line := scanner.Text()
 		index := strings.Index(line, ",")
 		id, err := strconv.Atoi(line[:index])
 		check(err)
-		ids = append(ids, id)
+		ids = append(ids, laneID(id))
 	}
 	return ids
 }
